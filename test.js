1c419db890efd4a01747f73ae17ddb7dd817757a
kon-pas <konpas@mail.com>
2022-01-07 22:03:46 +0100

    4

---
+++ test.js
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+// import fetch from 'node-fetch';
+
+// fetch('http://51.83.185.162:4000/attraction/all')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+=======
+import fetch from 'node-fetch';
+
+fetch('http://51.83.185.162:4000/attraction/all')
+  .then(response => response.json())
+  .then(data => console.log(data));
+>>>>>>> 4
